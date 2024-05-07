bb8a8199e891e65bbb6c04233c8ae17d26b7be38
michael <457813723@qq.com>
2024-05-08 00:17:56 +0800

    test

--- main.go
+++ main.go
@@ -7,9 +7,10 @@ import (
 func main() {
 	r := gin.Default()
 	r.GET("/test", func(c *gin.Context) {
-		c.JSON(200, "hello world")
+		c.JSON(200, "hello world 111111")
 	})
 	r.Run("0.0.0.0:8899") // 监听并在 0.0.0.0:8899 上启动服务
 }
 
 //CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build
+//docker build -f ./docker/Dockerfile -t go-test:v13 .
