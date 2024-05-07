d4add10e2e4166bbd52d844993ecc4c356e85ec9
michael <457813723@qq.com>
2024-05-07 23:58:21 +0800

    test

--- main.go
+++ main.go
@@ -9,5 +9,7 @@ func main() {
 	r.GET("/test", func(c *gin.Context) {
 		c.JSON(200, "hello world")
 	})
-	r.Run("127.0.0.1:8899") // 监听并在 0.0.0.0:8899 上启动服务
+	r.Run("0.0.0.0:8899") // 监听并在 0.0.0.0:8899 上启动服务
 }
+
+//CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build
